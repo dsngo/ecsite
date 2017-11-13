73824778c9bb3162158f07f378ff2624c607576f
Daniel <daniel.dungngo@gmail.com>
2017-11-13 14:26:11 +0700

    ready

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import FoF from "./FoF";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Navbar from "./Navbar";
+import Landing from "./Landing";
+
+export const App: React.SFC = () => (
+    <Router>
+            <MuiThemeProvider>
+                <div>
+                    <Navbar showSearch />
+                    <Switch>
+                        <Route exact path="/" component={Landing} />
+                        <Route component={FoF} />
+                    </Switch>
+                </div>
+            </MuiThemeProvider>
+    </Router>
+);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,47 @@
 import * as React from "react";
-import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { setSearchTerm } from "./redux/actionCreators";
+import { connect } from "react-redux";
 import TextField from "material-ui/TextField";
-// import
+import AppBar from "material-ui/AppBar";
+import Paper from "material-ui/Paper";
+import LoginPopup from "./LoginPopup";
+import SignupPopup from "./SignupPopup";
+import ShoppingCartIcon from "material-ui/svg-icons/action/shopping-cart";
+import { grey500 } from "material-ui/styles/colors";
 
-interface INavBar {
+interface INavbar {
+  isLogin: boolean;
+  username: string;
+  cartNumberOfItems: number;
 }
 
-const NavBar: React.SFC<INavBar> = props => 
+const Navbar: React.SFC<INavbar> = props => (
+  <Paper>
+    <Link to="/collections/womens-all">Women</Link>
+    <Link to="/collections/mens-all">Men</Link>
+    <Link to="/visit-us">Visit Us</Link>
+    <Link to="/factories">Factory</Link>
+    <Link to="/about">About</Link>
+    <Link to="/">ECSITE</Link>
+    {props.isLogin ? (
+      <Link to="/account/info">`Hi, ${props.username}`</Link>
+    ) : (
+      <div>
+        <LoginPopup />
+        <SignupPopup />
+      </div>
+    )}
+    <Link to="/checkout/preview">
+      <ShoppingCartIcon style={{marginRight: 12}}color={grey500} />
+      {props.cartNumberOfItems && `${props.cartNumberOfItems} ${props.cartNumberOfItems < 2 ? "item" : "items"}`}
+    </Link>
+  </Paper>
+);
 
+const mapStateToProps = (state: any) => ({
+  isLogin: state.isLogin,
+  username: state.username,
+  cartNumberOfItems: state.cartNumberOfItems,
+});
 
-export default Navbar;
+export default connect()(Navbar);
