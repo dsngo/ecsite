fc37b4b4f169b857fe4eb52a03fc57f237956f4f
Daniel <daniel.dungngo@gmail.com>
2017-11-13 13:33:30 +0700

    first commit

---
+++ src/components/Landing.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+// import { clearSubmitStatus } from "./redux/actionCreators";
+import { connect } from "react-redux";
+
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+interface ILanding {}
+
+const Landing: React.SFC<ILanding> = props => <div>THIS IS LANDING PAGE</div>;
+
+export default Landing;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { setSearchTerm } from "./redux/actionCreators";
+import TextField from "material-ui/TextField";
+// import
+
+interface INavBar {
+}
+
+const NavBar: React.SFC<INavBar> = props => 
+
+
+export default Navbar;
